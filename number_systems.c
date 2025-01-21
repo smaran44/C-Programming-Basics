ad8dff5925bb50e122b2186d8feaebea423de90d
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-21 12:40:46 +0530

    Update number_systems.c

--- number_systems.c
+++ number_systems.c
@@ -1,6 +1,37 @@
  #include <stdio.h>
 
  int main() {
+
+    /*In programming, four main number systems are commonly used. Each has a specific purpose depending on the context:
+
+    1. Decimal (Base-10)
+       The number system we use in daily life. It uses 10 digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+      Decimal: 0, 1, 2, 10, 255, 1024, etc.
+
+    2. Binary (Base-2)
+    The language of computers. It uses 2 digits: 0 and 1
+    Binary: 0, 1, 10 (2 in decimal), 1010 (10 in decimal), etc.
+
+     binary to decimal:-
+    
+    Binary:      1      0      1      1
+    Positions:   3      2      1      0
+    Powers:      2³     2²     2¹     2⁰
+    Calculation: 1×8  + 0×4  + 1×2  + 1×1
+            = 8   + 0    + 2    + 1
+            = 11 (Decimal)
+
+     
+     decimal to binary:-
+
+    Step 1: 11 ÷ 2 = 5 remainder 1
+    Step 2: 5 ÷ 2 = 2 remainder 1
+    Step 3: 2 ÷ 2 = 1 remainder 0
+    Step 4: 1 ÷ 2 = 0 remainder 1
+*/
+
+
+    
     
     // %x is used for hexadecimal values
 

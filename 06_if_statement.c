922c6b3337a647f7515d03ed3194c6e88a503f52
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-21 20:29:40 +0530

    Create 06_if_statement.c

---
+++ 06_if_statement.c
@@ -0,0 +1,34 @@
+// conditional statements are used to perform different actions based on different conditions
+// there are generally four types of conditional statements
+// 1. if statement
+// 2. if else statement
+// 3. else if statement
+// 4. switch statement
+
+#include <stdio.h>
+int main() {
+
+    // if statement
+    // if statement is used to execute a block of code only if the condition is true
+
+    int a,b;
+    printf("Enter the value of a : ");
+    scanf("%d",&a);
+    printf("Enter the value of b : ");
+    scanf("%d",&b);
+
+    if (a>b) {
+        printf("a is greater than b\n");
+    }
+    
+    /*Q:- find positive numbers*/
+    int num;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+
+    if (num>0) {
+        printf("the number is positive\n");
+    }
+
+    return 0;
+}

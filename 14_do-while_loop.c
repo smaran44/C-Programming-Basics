d7601036ff51f9043b3b2a84fc8871edf83451a7
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-22 16:37:12 +0530

    Create 14_do-while_loop.c

---
+++ 14_do-while_loop.c
@@ -0,0 +1,50 @@
+/*
+do-while loop
+The do-while loop is similar to the while loop, but the condition is checked after the execution of the block of code.
+ This means that the block of code will always be executed at least once, even if the condition is false.
+Syntax:
+do {
+// Code to be executed
+} while (condition);
+*/
+
+#include <stdio.h>
+int main() {
+
+    // here the major difference b/w while and do-while is in do-while block of code will always be executed at least once
+
+    int a =1;
+    do {
+        printf("%d\n",a);
+        a++;
+    } while (a<1);
+
+// Here in do-while loop the condition is false but the block of code is executed once
+
+
+    int i = 1;
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 10);
+
+    // print even numbers from 1 to 10
+    int j = 1;
+    do {
+        if (j % 2 == 0) {
+            printf("%d\n", j);
+        }
+        j++;
+    } while (j <= 10);
+
+    // print odd numbers from 1 to 10
+    int k = 1;
+    do {
+        if (k % 2 != 0) {
+            printf("%d\n", k);
+        }
+        k++;
+    } while (k <= 10);
+
+    return 0;
+}

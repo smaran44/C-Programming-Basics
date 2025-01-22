3d9364ef1214bff89881012b93915541b742bdb9
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-22 13:52:12 +0530

    Update 07_if_else_statement.c

---
+++ 07_if_else_statement.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main() {
+
+    // if else statement
+    // if else statement is used to execute a block of code if the condition is true and another block of code if the condition is false
+
+  //syntax:
+  /*if (condition) {
+    // Code to execute if condition is true
+ } else {
+    // Code to execute if condition is false
+ } */
+
+    int a,b;
+    printf("Enter the value of a : ");
+    scanf("%d",&a);
+    printf("Enter the value of b : ");
+    scanf("%d",&b);
+
+    if (a>b) {
+        printf("a is greater than b\n");
+    } else {
+        printf("b is greater than a\n");
+    }
+
+    /*Q:- find positive numbers*/
+    int num;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+
+    if (num>0) {
+        printf("the given number %d is positive\n",num);
+    } else {
+        printf("the given number %d is not positive\n",num);
+    }
+
+    return 0;
+}

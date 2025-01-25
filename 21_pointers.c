02ff2e72492b0b3ed3410b4b9eb79a3996e0cb65
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-25 16:23:20 +0530

    Update 21_pointers.c

--- 21_pointers.c
+++ 21_pointers.c
@@ -1,9 +1,4 @@
 #include<stdio.h>
-
-void square(int n);
-void _square(int* n);
-
-
 int main() {
 int age = 22;
 int *ptr = &age;
@@ -16,25 +11,6 @@ printf("%p\n", &ptr);
 //data
 printf("%d\n", age);
 printf("%d\n", *ptr);
-printf("%d\n", *(&age));
-
-int number = 4;
-//call by value
-square(number);
-printf("n is : %d\n", number);
-//call by reference
-_square(&number);
-printf("n is : %d\n", number);
+printf("%d\n", *(&age));//*(&age) = age
 return 0;
 }
-void square(int n) {
-n = n * n;
-printf("square is : %d\n", n);
-}
-void _square(int* n) {
-*n = *n * *n;
-printf("square is : %d\n", *n);
-}
-
-
-return 0;

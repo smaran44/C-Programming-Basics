b97636953025ca8a23cec82667445b4214d3a669
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-26 15:05:30 +0530

    Create 24_void_function.c

---
+++ 24_void_function.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//function declaration/prototype
+void printHello();
+int main() {
+//function call
+printHello();
+return 0;
+}
+//function definition
+void printHello() {
+printf("Hello!\n");
+}
